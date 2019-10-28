cfd4484d65704b38337d8edab8759029145eff7e
arianrah <arianrahimi@icloud.com>
2019-10-28 10:39:20 -0700

    Added button logic and styling, ran tests, updated node sass.

--- stories/index.js
+++ stories/index.js
@@ -15,7 +15,7 @@ storiesOf("Button", module)
   .add("Confirm", () => <Button confirm>Confirm</Button>)
   .add("Danger", () => <Button danger>Cancel</Button>)
   .add("Clickable", () => (
-    <Button onClick={action("button-clicked")}>Clickable</Button>
+    <Button clickable onClick={action("button-clicked")}>Clickable</Button>
   ))
   .add("Disabled", () => (
     <Button disabled onClick={action("button-clicked")}>
