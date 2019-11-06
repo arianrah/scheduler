d82946abb55e699bf7883c79ad92226308d8c557
arianrah <arianrahimi@icloud.com>
2019-11-06 12:42:27 -0800

    Fixed article tag wrap bug.

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -62,38 +62,43 @@ export default function Appointment(props) {
 
   return (
     <React.Fragment>
-      <article className="appointment"></article>
-      <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && (
-        <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-          onEdit={edit}
-          onDelete={cancel}
-        />
-      )}
-      {mode === CREATE && (
-        <Form interviewers={props.interviewers} onCancel={back} onSave={save} />
-      )}
-      {mode === SAVING && <Status message="Saving" />}
-      {mode === CONFIRM && <Confirm onCancel={back} onConfirm={destroy} />}
-      {mode === DELETE && <Status message="Deleting" />}
-      {mode === EDIT && (
-        <Form
-          name={props.interview.student}
-          interviewers={props.interviewers}
-          interviewer={props.interview.interviewer.id}
-          onCancel={back}
-          onSave={save}
-        />
-      )}
-      {mode === ERRCREATE && (
-        <Error message="Could not create appointment" onClose={back} />
-      )}
-      {mode === ERRDELETE && (
-        <Error message="Could not delete appointment" onClose={back} />
-      )}
+      <article className="appointment">
+        <Header time={props.time} />
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+            onEdit={edit}
+            onDelete={cancel}
+          />
+        )}
+        {mode === CREATE && (
+          <Form
+            interviewers={props.interviewers}
+            onCancel={back}
+            onSave={save}
+          />
+        )}
+        {mode === SAVING && <Status message="Saving" />}
+        {mode === CONFIRM && <Confirm onCancel={back} onConfirm={destroy} />}
+        {mode === DELETE && <Status message="Deleting" />}
+        {mode === EDIT && (
+          <Form
+            name={props.interview.student}
+            interviewers={props.interviewers}
+            interviewer={props.interview.interviewer.id}
+            onCancel={back}
+            onSave={save}
+          />
+        )}
+        {mode === ERRCREATE && (
+          <Error message="Could not create appointment" onClose={back} />
+        )}
+        {mode === ERRDELETE && (
+          <Error message="Could not delete appointment" onClose={back} />
+        )}
+      </article>
     </React.Fragment>
   );
 }
