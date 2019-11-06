7727d9460f25566884d653dcd711076ccd62c9e2
arianrah <arianrahimi@icloud.com>
2019-11-06 13:37:15 -0800

    Fixed props usage scope.

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -22,7 +22,6 @@ export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
-
   function save(name, interviewer) {
     const interview = {
       student: name,
