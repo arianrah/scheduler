5eb0db9c647ff578cef9e1a2691839dd53d7337d
arianrah <arianrahimi@icloud.com>
2019-11-06 09:30:48 -0800

    Added empty student name check. (Validate function)

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,12 @@
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from "prop-types";
+
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+};
 
 export default function InterviewerList(props) {
   const InterviewerListRender = props.interviewers.map(interviewer => {
